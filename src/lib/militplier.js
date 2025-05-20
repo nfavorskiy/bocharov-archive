fd10d1084cfcc699bc250e249ad580671c12a66f
Nikolay Favorskiy <lordnikolas9091@gmail.com>
2025-05-20 15:52:32 +0200

    Add multiply function as an example for testing

---
+++ src/lib/militplier.js
@@ -0,0 +1,19 @@
+
+let multiply = (a, b) => {
+    return a * b;
+};
+
+// const x = '';
+
+
+// function a(x) {
+//     if (true) {
+//         return x;
+//     } else if (false) {
+//         return x+1;
+//     } else {
+//         return 4; // 3rd path
+//     }
+// }
+
+export default multiply;
