5dcd621313e9058a251fb11d8d8d78d6adda6c03
Nikolay Favorskiy <lordnikolas9091@gmail.com>
2026-07-28 16:19:36 +0200

    add diploma1951 section

---
+++ src/routes/projects/diploma1951/+page.server.js
@@ -0,0 +1,16 @@
+import { buildGalleryImages } from '$lib/galleryServer';
+
+const DIR = 'static/images/diploma1951';
+const THUMB_URL_BASE = '/imagesThumbs/diploma1951';
+const FULL_URL_BASE = '/images/diploma1951';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load() {
+  const images = await buildGalleryImages({
+    dir: DIR,
+    thumbBase: THUMB_URL_BASE,
+    fullBase: FULL_URL_BASE
+  });
+
+  return { images };
+}
