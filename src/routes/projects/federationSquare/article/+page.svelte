b2c8fbde1a36d8a84bdcd458fed5844a4fc4caec
Nikolay Favorskiy <lordnikolas9091@gmail.com>
2026-07-31 15:47:06 +0200

    remove numbers from names of documents

--- src/routes/projects/federationSquare/article/+page.svelte
+++ src/routes/projects/federationSquare/article/+page.svelte
@@ -2,4 +2,7 @@
   import PDFViewer from '$lib/components/PDFViever.svelte';
 </script>
 
-<PDFViewer src="/documents/14. Газета_ру О проекте площади на месте гост Москва. 2008.pdf" title="14. Газета_ру О проекте площади на месте гост Москва. 2008" />
+<PDFViewer 
+  src="/documents/Газета_ру О проекте площади на месте гост Москва. 2008.pdf" 
+  title="Газета_ру О проекте площади на месте гост Москва. 2008" 
+/>
--- src/routes/projects/federationSquare/idea/+page.svelte
+++ src/routes/projects/federationSquare/idea/+page.svelte
@@ -2,4 +2,7 @@
   import PDFViewer from '$lib/components/PDFViever.svelte';
 </script>
 
-<PDFViewer src="/documents/1. Об идее проекта новой площади.pdf" title="1. Об идее проекта новой площади" />
+<PDFViewer 
+  src="/documents/Об идее проекта новой площади.pdf" 
+  title="Об идее проекта новой площади" 
+/>
