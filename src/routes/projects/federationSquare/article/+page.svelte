dba67c94725c580007a7520bac449ed52f426965
Nikolay Favorskiy <lordnikolas9091@gmail.com>
2026-07-31 15:44:15 +0200

    add article to federationSquare project section

---
+++ src/routes/projects/federationSquare/article/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import PDFViewer from '$lib/components/PDFViever.svelte';
+</script>
+
+<PDFViewer src="/documents/14. Газета_ру О проекте площади на месте гост Москва. 2008.pdf" title="14. Газета_ру О проекте площади на месте гост Москва. 2008" />
