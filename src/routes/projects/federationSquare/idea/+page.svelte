8bf3ed282f0206e14c2270fed4837535b3b84c6e
Nikolay Favorskiy <lordnikolas9091@gmail.com>
2026-07-31 15:45:51 +0200

    add idea page to the federationSquare project section

---
+++ src/routes/projects/federationSquare/idea/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import PDFViewer from '$lib/components/PDFViever.svelte';
+</script>
+
+<PDFViewer src="/documents/1. Об идее проекта новой площади.pdf" title="1. Об идее проекта новой площади" />
