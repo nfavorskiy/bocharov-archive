ace2e707eed336e2b568ba3ef10ed99fbb3ce615
Nikolay Favorskiy <lordnikolas9091@gmail.com>
2026-08-10 17:29:38 +0200

    add defenseAnnouncement page

---
+++ src/routes/research/defenseAnnouncement/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import ImageBox from "$lib/components/ImageBox.svelte";
+
+</script>
+<article>
+    <ImageBox 
+        src="/images/Объявление о защите 1967.jpg" 
+        alt="Объявление о защите 1967" 
+        caption="Объявление о защите 1967" 
+    />
+</article>
+
+<style>
+    article {
+        padding: 0rem 2rem 1rem 2rem;
+        line-height: 1.8;
+    }
+
+</style>
